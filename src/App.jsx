5c1cf6030e9dfbd58503e82a2d3625458ece0d0c
ariannespg <Arianne.perretgentil@correo.unimet.edu.ve>
2024-10-18 20:46:20 -0400

    Carpeta Pages y otros detalles
    Se crearon algunas páginas y se editó el código inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomeReservas from "./Pages/HomeReservas";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+
+
+function App() {
+  return <div className='App'> 
+      
+      </div>
+}
+
+
+export default App
---
+++ src/Pages/HomeReservas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeReservas = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeReservas
