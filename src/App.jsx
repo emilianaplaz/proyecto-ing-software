13273197c292c864c66b018628a7f001974cbcc7
ariannespg <Arianne.perretgentil@correo.unimet.edu.ve>
2024-10-30 12:08:49 -0400

    Acomodo de App root

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       {/* <HeaderLanding /> */}
       <Routes>
         {/* Set LandingPage as the default route */}
-        <Route path="/" element={<HomeReservas/>} />
+        <Route path="/" element={<LandingPage/>} />
         {/* Define other routes like HomeReservas and SignUp */}
         <Route path="/reservas" element={<HomeReservas />} />
         <Route path="/signup" element={<SignUp />} />
