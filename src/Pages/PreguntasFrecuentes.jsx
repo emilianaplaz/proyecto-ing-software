3a196d1bc5c3c138cfee4b84f38353c7a52784b9
emilianaplaz <emiplaz@gmail.com>
2024-11-25 14:31:47 -0400

    preguntas frecuentes

--- src/Pages/PreguntasFrecuentes.jsx
+++ src/Pages/PreguntasFrecuentes.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
 import Header from '../Components/Header'
-import './PreguntasFrecuentes.css'; 
+import './PreguntasFrecuentes.css'; // Importa los estilos desde el archivo CSS
 
 const PreguntasFrecuentes = () => {
-  return (
-    <div>
-      <Header />
-        <div className='container-preguntas-frecuentes'>
-        <div className="title" >Preguntas Frecuentes</div>
-        </div>
-    </div>
-  )
-}
+  const preguntas = [
+    {
+      pregunta: "¿Si soy estudiante necesito permiso para reservar un aula?",
+      respuesta:
+        "Sí, los estudiantes deben esperar a que el administrador les otorgue el permiso de reserva de aula para que su reserva sea procesada.",
+    },
+    {
+      pregunta: "¿Cualquier estudiante puede reservar un aula?",
+      respuesta:
+        "No, solo aquellos que tengan una razón válida como preparadores y miembros de agrupaciones.",
+    },
+    {
+      pregunta: "¿Cuánto tiempo se tarda en procesar una solicitud?",
+      respuesta:
+        "Se recomienda hacer la solicitud con 3 días de antelación, sin embargo, puede ser procesada al momento.",
+    },
+  ];
 
-export default PreguntasFrecuentes
+  return (
+    <div className="container">
+      <Header />
+      <header className="header">
+        <h1 className="title">Preguntas Frecuentes</h1>
+      </header>
+      <div className="content">
+        {preguntas.map((item, index) => (
+          <div key={index} className="question-block">
+            <h3 className="question">{item.pregunta}</h3>
+            <p className="answer">{item.respuesta}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PreguntasFrecuentes;
